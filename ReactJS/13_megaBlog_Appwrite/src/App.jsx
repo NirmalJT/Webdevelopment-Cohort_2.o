a7e6d314a082de4cb510a25df90221e308d4374f
NirmalJT <nirmaljyotithakuria@gmail.com>
2025-04-06 00:12:58 +0530

    setup redux tollkit store for login and logout

--- ReactJS/13_megaBlog_Appwrite/src/App.jsx
+++ ReactJS/13_megaBlog_Appwrite/src/App.jsx
@@ -3,13 +3,39 @@ import { useDispatch } from "react-redux";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import "./App.css";
+import { Outlet } from "react-router-dom";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
 
 function App() {
-  return (
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login());
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, [dispatch]);
+
+  return !loading ? (
     <>
-     <div></div>
+      <div className="">
+        <div>
+          <Header />
+          <main>
+            Main: <Outlet />
+          </main>
+          <Footer />
+        </div>
+      </div>
     </>
-  );
+  ) : null;
 }
 
 export default App;
