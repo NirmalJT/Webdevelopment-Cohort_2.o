0e29a4bbc9041538e00a40a732d81977d142aabf
NirmalJT <nirmaljyotithakuria@gmail.com>
2025-04-09 22:55:15 +0530

    make some authlayer component

---
+++ ReactJS/13_megaBlog_Appwrite/src/components/RTE.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Editor } from "@tinymce/tinymce-react";
+import { Controller } from "react-hook-form";
+function RTE({ label, control, name, defaulValue = "" }) {
+  return (
+    <>
+      <div>
+        {label && <label>{label}</label>}
+        <Controller
+          name={name || "content"}
+          control={control}
+          render={({ field: { onChange } }) => (
+            <Editor
+              initialValue={defaulValue}
+              init={{
+                branding: false,
+                height: 500,
+                menubar: true,
+                plugins: [
+                  "advlist",
+                  "autolink",
+                  "lists",
+                  "link",
+                  "image",
+                  "charmap",
+                  "preview",
+                  "anchor",
+                  "searchreplace",
+                  "visualblocks",
+                  "code",
+                  "fullscreen",
+                  "insertdatetime",
+                  "media",
+                  "table",
+                  "code",
+                  "help",
+                  "wordcount",
+                ],
+                toolbar:
+                  "undo redo | blocks | " +
+                  "bold italic forecolor | alignleft aligncenter " +
+                  "alignright alignjustify | bullist numlist outdent indent | " +
+                  "removeformat | help",
+                content_style:
+                  "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+              }}
+              onEditorChange={onChange}
+            />
+          )}
+        />
+      </div>
+    </>
+  );
+}
+
+export default RTE;
